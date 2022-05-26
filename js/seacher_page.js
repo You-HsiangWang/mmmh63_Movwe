72728d42a1b393fe2b192ec00ee1cdd1ead33dfc
name <100788689+DinoWangjojo@users.noreply.github.com>
2022-05-26 17:23:50 +0800

    0526

--- js/seacher_page.js
+++ js/seacher_page.js
@@ -14,24 +14,81 @@ const carouselBigBox__7 = document.querySelector('.carousel__big__box__7')
 
 articleContainer.style.display = 'none'
 forumButton.addEventListener('click',()=>{
+
+    if(forumButton.style.backgroundColor === 'rgb(16, 255, 162)'){
+        forumButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+        forumButton.style.color = 'rgba(255, 255, 255, 0.3)'
+    }
+    else{
+        forumButton.style.backgroundColor = 'rgb(16, 255, 162)'
+        forumButton.style.color = 'black'
+
+    }
+
     articleContainer.style.display = 'block'
     carouselBigBox.style.display = 'none'
     carouselBigBox__7.style.display = 'none'
 
+    movieButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    movieButton.style.color = 'rgba(255, 255, 255, 0.3)'
+
+    productButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    productButton.style.color = 'rgba(255, 255, 255, 0.3)'
+
+
 })
 
+
+movieButton.style.backgroundColor = 'rgb(16, 255, 162)'
+movieButton.style.color = 'black'
 movieButton.addEventListener('click',()=>{
+    if(movieButton.style.backgroundColor === 'rgb(16, 255, 162)'){
+        movieButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+        movieButton.style.color = 'rgba(255, 255, 255, 0.3)'
+    }
+    else{
+        movieButton.style.backgroundColor = 'rgb(16, 255, 162)'
+        movieButton.style.color = 'black'
+
+    }
     carouselBigBox.style.display = 'block'
     articleContainer.style.display = 'none'
     carouselBigBox__7.style.display = 'none'
+
+    forumButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    forumButton.style.color = 'rgba(255, 255, 255, 0.3)'
+
+    productButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    productButton.style.color = 'rgba(255, 255, 255, 0.3)'
+
+
 
 })
 
 carouselBigBox__7.style.display = 'none'
 productButton.addEventListener('click',()=>{
+    if(productButton.style.backgroundColor === 'rgb(16, 255, 162)'){
+        productButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+        productButton.style.color = 'rgba(255, 255, 255, 0.3)'
+    }
+    else{
+        productButton.style.backgroundColor = 'rgb(16, 255, 162)'
+        productButton.style.color = 'black'
+
+    }
+
+
     carouselBigBox__7.style.display = 'block'
     carouselBigBox.style.display = 'none'
     articleContainer.style.display = 'none'
+
+    forumButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    forumButton.style.color = 'rgba(255, 255, 255, 0.3)'
+
+    movieButton.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    movieButton.style.color = 'rgba(255, 255, 255, 0.3)'
+
+
 })
 
 
