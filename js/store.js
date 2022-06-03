c81b255c8df29d16d2072277302631ad43cf5866
haita <usamaruchi@gmail.com>
2022-06-03 16:49:51 +0800

    0603_ver2.2_商城排版、圖片修改_瀚

--- js/store.js
+++ js/store.js
@@ -14,31 +14,44 @@ const storeMarvel = document.querySelector('.store__marvel')
 storeStar.style.display = 'none'
 storeDis.style.display = 'none'
 storeMarvel.style.display = 'none'
-mainBtn.addEventListener('click',()=>{
+mainBtn.addEventListener('click', () => {
+
     storeMain.style.display = 'block'
     storeStar.style.display = 'none'
     storeDis.style.display = 'none'
     storeMarvel.style.display = 'none'
 })
 
-starBtn.addEventListener('click',()=>{
+starBtn.addEventListener('click', () => {
     storeMain.style.display = 'none'
     storeStar.style.display = 'block'
     storeDis.style.display = 'none'
     storeMarvel.style.display = 'none'
 })
 
-disBtn.addEventListener('click',()=>{
+disBtn.addEventListener('click', () => {
     storeMain.style.display = 'none'
     storeStar.style.display = 'none'
     storeDis.style.display = 'block'
     storeMarvel.style.display = 'none'
 })
 
-marvelBtn.addEventListener('click',()=>{
+marvelBtn.addEventListener('click', () => {
     storeMain.style.display = 'none'
     storeStar.style.display = 'none'
     storeDis.style.display = 'none'
     storeMarvel.style.display = 'block'
 })
 
+
+// $('.__btn').mouseenter(function () {
+   
+//     $(this).css('box-shadow', '0 0 10px #10FFA2');
+// }
+// )
+
+$('.__btn').click(function () {
+    $(this).css('box-shadow', '0 0 10px #10FFA2');
+
+    $(this).siblings().css('box-shadow', 'none');
+})
